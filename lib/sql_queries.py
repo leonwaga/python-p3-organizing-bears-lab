ab1153907d0b32cbc666ca021420a33cb26334b2
leon <leon.waga@student.moringaschool.comgit config --global user.name leongit config --global user.name leon>
2023-09-26 03:10:04 +0300

    completed assignmnet

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,18 +1,40 @@
 select_all_female_bears_return_name_and_age = """
-    Write your SQL query here
+    SELECT
+        bears.name,
+        bears.age
+    FROM bears
+    WHERE sex='F';
 """
 
 select_all_bears_names_and_orders_in_alphabetical_order = """
-    Write your SQL query here
+    SELECT
+        bears.name
+    FROM bears
+    ORDER BY bears.name DESC
 """
 
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    Write your SQL query here
+    SELECT
+        bears.name,
+        bears.age,
+        bears.alive,
+    FROM bears
+    ORDER BY bears.age ASC
 """
 
 select_oldest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+    SELECT
+        bears.age
+    FROM bears
+    ORDER BY bones.age DESC
+    LIMIT 1
+    RETURN(bears.name, bears.age)
 """
 select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+    SELECT
+        bears.age
+    FROM bears
+    ORDER BY bones.age ASC
+    LIMIT 1
+    RETURN(bears.name, bears.age)
 """
